411c738ceef020350f3a221785a74553cdcd4b28
Jorge Ossa <jorgehossa@gmail.com>
2020-01-17 00:50:38 -0500

    config: call express, socket.io and johnny-five dependencies

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import './scss/main.scss';
+
+const express = require('express');
+
+const app = express();
+// const io = require('socket.io')(app.listen(8081));
+const five = require('johnny-five');
+
+app.use(express.static(`${__dirname}/app`));
+
+app.get('/', function(req, res) {
+  res.sendFile(`${__dirname}/index.html`);
+});
