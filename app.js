77d7d5db7101ed1f83015970161da577ea8d19d6
Jorge Ossa <jorgehossa@gmail.com>
2020-01-24 11:37:07 -0500

    config: config express to serve static files and add css file

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const Bundler = require('parcel-bundler');
 const Path = require('path');
 
 const app = express();
 
-const entryFiles = Path.join(__dirname, './index.html');
-
-(async function() {
-  // Initializes a bundler using the entrypoint location and options provided
-  const bundler = new Bundler(entryFiles, options);
-
-  // Run the bundler, this returns the main bundle
-  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
-  const bundle = await bundler.bundle();
-})();
+app.use(express.static('src'));
 
 app.get('/', function(req, res) {
-  res.send({ hello: 'World' });
+  res.sendFile(`${__dirname}/src/index.html`);
 });
 
 const server = app.listen(3001, function() {
