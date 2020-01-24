99b2388af771c8e8aa7ebc04ae41cf62db7de5a0
Jorge Ossa <jorgehossa@gmail.com>
2020-01-24 15:14:19 -0500

    config: try to config socket.io

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const Path = require('path');
 
 const app = express();
 
+const io = require('socket.io')(app.listen(8081));
+
 app.use(express.static('src'));
 
 app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/src/index.html`);
+});
+
+io.on('conection', function(socket) {
+  socket.on('green', function() {
+    console.log('Green checkbox active');
+  });
 });
 
 const server = app.listen(3001, function() {
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hola Server');
+const greenSwitch = document.querySelector('.switch.green input');
+const blueSwitch = document.querySelector('.switch.blue input');
+const yellowSwitch = document.querySelector('.switch.yellow input');
+const socket = io();
+
+if (greenSwitch.checked) {
+  socket.emmit('green');
+}
